fe2dedeee087b2c89a3fa8d11a8be3d6f52ab9a3
Willis Allstead <willis.allstead@noblestudios.com>
2022-03-09 15:49:29 -0800

    Rename basepath for vite before pushing to github

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,6 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  base: "/react-interview-exercise/"
+  base: "/school-search/"
 })
 
